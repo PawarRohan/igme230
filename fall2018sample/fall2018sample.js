2e69f31c81557862ee9a0c741fb2777ee8d923ee
PawarRohan <rkp9673@g.rit.edu>
2018-12-06 13:24:45 -0500

    skoot

---
+++ fall2018sample/fall2018sample.js
@@ -0,0 +1,18 @@
+/* Don't use <script> tags in a linked js file! */
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+// ajax
+let selection = ("content1.txt")
+$("#choose-content").val(selection);
+$("#content").load(selection);
+
+$("choose-content").change(function(){
+    selection = $(this).val();
+    $("#content").load(selection);
+
+}); 
+
+
+//jquery
+$(".menubox").click(function(){
+    $(this).find(".submenu").slideToggle();
+});
